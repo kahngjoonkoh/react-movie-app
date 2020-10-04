4828632199849972cca5c97d81d1643de2637f92
Kahngjoon Koh <kahngjoonk@gmail.com>
2020-10-04 23:38:45 +0400

    Displays to web + genres

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 //This file does not need "state" Therefore, it doesn't require a class component and can be used with a function component
-function Movie({ id, year, title, summary, poster }) {
-    return <h4>{title}</h4>;
-
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default Movie; 
+export default Movie;
