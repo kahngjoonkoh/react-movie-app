20f4c512d4d465d01736924cb36ace684799855b
Kahngjoon Koh <kahngjoonk@gmail.com>
2020-10-05 20:47:55 +0400

    Updates

---
+++ precache-manifest.2f9e6a18dfa213da60c1ef7bb8dc09b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70251008872e63f744e4ac99f0c1ebc1",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "f4011fd29491e454cf5c",
+    "url": "/react-movie-app/static/css/main.1def891f.chunk.css"
+  },
+  {
+    "revision": "9f80a518c59343a92732",
+    "url": "/react-movie-app/static/js/2.6e20bb24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.6e20bb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4011fd29491e454cf5c",
+    "url": "/react-movie-app/static/js/main.b16ef31d.chunk.js"
+  },
+  {
+    "revision": "651af530d224a688009d",
+    "url": "/react-movie-app/static/js/runtime-main.c0625075.js"
+  }
+]);
